5958311fc93a7aafbbae9b34d065cc6fccf3cb6a
guandezhi <guandezhi@baibu.la>
2020-12-10 14:10:37 +0800

    feat:项目初始化

---
+++ src/main/java/com/example/sourcetreedemo/SourcetreedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sourcetreedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcetreedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourcetreedemoApplication.class, args);
+    }
+
+}
